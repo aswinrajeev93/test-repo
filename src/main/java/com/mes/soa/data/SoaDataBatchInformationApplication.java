7b939ac208903c4c503cbeba4c2cd7566294b609
aswinrajeev93 <54107562+aswinrajeev93@users.noreply.github.com>
2019-12-05 17:29:15 +0530

    Update SoaDataBatchInformationApplication.java

--- src/main/java/com/mes/soa/data/SoaDataBatchInformationApplication.java
+++ src/main/java/com/mes/soa/data/SoaDataBatchInformationApplication.java
@@ -2,12 +2,15 @@ package com.mes.soa.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import software.amazon.codeguruprofilerjavaagent.Profiler;
 @SpringBootApplication
 public class SoaDataBatchInformationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoaDataBatchInformationApplication.class, args);
+		new Profiler.Builder()
+   .profilingGroupName("test-Deployment")
+   .build().start()
 	}
 
 }
